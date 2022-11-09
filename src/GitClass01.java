e8b6f6e27c3d540c40d25f69a3bbee9c663f3e27
LeopoldHolgersson <leopold.holgersson@gmail.com>
2022-11-09 13:37:03 +0100

    Added a new class and method.

---
+++ src/GitClass01.java
@@ -0,0 +1,8 @@
+public class GitClass01 {
+
+    public void printGit() {
+
+        System.out.println("Git is gut.");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+        GitClass01 gitClass01 = new GitClass01();
+
+        System.out.println("Hello Git!");
+
+        gitClass01.printGit();
+
+
     }
 }
